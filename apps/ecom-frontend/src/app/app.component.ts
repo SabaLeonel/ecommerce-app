935aaa62db315c1e41e86751d5e40b281d1096dd
sabaleonel <leonel.saba@hotmail.com>
2025-02-15 22:54:57 +0100

    Implement footer and navbar components with SCSS styling; update app structure and FontAwesome icons

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,24 +1,32 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { FaConfig, FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
-  imports: [ RouterModule],
-  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterModule,
+    FaIconComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-    private faIconLibrary = inject(FaIconLibrary);
-    private faConfig = inject(FaConfig);
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
 
-    ngOnInit(): void {
-      this.initFontAwesome();
-    }
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
 
-    private initFontAwesome(): void {
-      this.faConfig.defaultPrefix = 'far';
-      this.faIconLibrary.addIcons(...fontAwesomeIcons);
-    }
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
