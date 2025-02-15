69a8e9f8f2edf7d8e6f712fd0873992322206cbe
sabaleonel <leonel@structo.ch>
2025-02-15 09:48:38 +0100

    need to fix error on import for fontawesome icons

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   imports: [ RouterModule],
@@ -7,6 +9,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit {
+    private faIconLibrary = inject(FaIconLibrary);
+    private faConfig = inject(FaConfig);
+
+    ngOnInit(): void {
+      this.initFontAwesome();
+    }
+
+    private initFontAwesome(): void {
+      this.faConfig.defaultPrefix = 'far';
+      this.faIconLibrary.addIcons(...fontAwesomeIcons);
+    }
 }
